4f2ee0500480fe6cab867e38b702855957440765
Priyanshu Ahuja <26710303+Priyanshu-001@users.noreply.github.com>
2023-06-20 17:50:40 +0530

    added default get_img function

--- VGG_16.py
+++ VGG_16.py
@@ -8,16 +8,19 @@ from tensorflow.keras.preprocessing import image
 base_model = VGG16(weights='imagenet', include_top=False)
 
 # Create a new model that outputs the last convolutional layer's output
-model = Model(inputs=base_model.input, outputs=base_model.get_layer('block5_pool').output)
+model = Model(inputs=base_model.input,
+              outputs=base_model.get_layer('block5_pool').output)
 
-def img_from_disk(img_path:str):
-    img = image.load_img(img_path, target_size=(224, 224))  # VGG-16 expects images of size 224x224
+
+def img_from_disk(img_path: str):
+    # VGG-16 expects images of size 224x224
+    img = image.load_img(img_path, target_size=(224, 224))
     return img
 
 
-def extract_features(get_img,img_path):
+def extract_features(img_path, get_img=img_from_disk):
     img = get_img(img_path)
     x = image.img_to_array(img)
     x = preprocess_input(x)
     x = np.expand_dims(x, axis=0)
-    return  model.predict(x)
+    return model.predict(x)
